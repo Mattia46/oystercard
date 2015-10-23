34663fc973f779ca9ae0eac7fe89a5ceabba1421
Roi Driscoll <roi.driscoll@gmail.com>
2015-10-23 15:11:46 +0100

    first refactoring

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,5 +1,5 @@
 require './lib/journey'
-require './lib/station'
+# require './lib/station'
 
 describe Journey do
   let(:station1) {Station.new 'Vic',1}
@@ -10,19 +10,19 @@ describe Journey do
       expect(journey1.entry).to eq station1
     end
     it 'exit station' do
-      journey1.exit = station2
-      expect(journey1.exit).to eq station2
+      journey1.exit_station = station2
+      expect(journey1.exit_station).to eq station2
     end
     it 'set fare' do
-      journey1.exit = station2
-      faretest = (journey1.entry.zone - journey1.exit.zone).abs * 2
+      journey1.exit_station = station2
+      faretest = ((journey1.entry.zone - journey1.exit_station.zone).abs + Journey::MIN_FARE)
       expect(journey1.fare).to eq faretest
     end
     it 'set journey not complete' do
       expect(journey1.complete).to eq false
     end
     it 'journey  complete' do
-      journey1.exit = station2
+      journey1.exit_station = station2
       expect(journey1.complete).to eq true
     end
   end
